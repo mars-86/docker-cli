8ead1f9b90692183756a04270c3d9c60669bcdaf
mars-86 <hrslash86@gmail.com>
2023-03-29 22:11:29 -0300

    refactor: init module

--- init/inc/process.h
+++
@@ -1,6 +0,0 @@
-#ifndef __DOCKER_CLI_INIT_PROCESS_INCLUDED_H__
-#define __DOCKER_CLI_INIT_PROCESS_INCLUDED_H__
-
-int init_daemon(const char *path, char *args, PROCESS_INFORMATION *proc);
-
-#endif // __DOCKER_CLI_INIT_PROCESS_INCLUDED_H__
--- init/init.h
+++ init/init.h
@@ -1,7 +1,6 @@
 #ifndef __DOCKER_CLI_INIT_INCLUDED_H__
 #define __DOCKER_CLI_INIT_INCLUDED_H__
 
-#include "inc/process.h"
-#include "inc/gui.h"
+#include "../constants/inc/error_codes.h"
 
 #endif // __DOCKER_CLI_INIT_INCLUDED_H__

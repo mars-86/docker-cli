723ece035f8fdb1a2985225e17f238716cc515e3
mars-86 <hrslash86@gmail.com>
2023-06-20 14:51:07 -0300

    feat: daemon healthcheck

--- gui/src/thread.c
+++ gui/src/thread.c
@@ -3,7 +3,7 @@
 #include <windows.h>
 #include "../inc/thread.h"
 
-void *start_daemon(void *args)
+void *daemon_start(void *args)
 {
     const char *cmd = (const char *)args;
 
@@ -11,7 +11,7 @@ void *start_daemon(void *args)
     return NULL;
 }
 
-DOCKERCLI_CODE init_daemon(const char *path, char *args, pthread_t *tid)
+DOCKERCLI_CODE daemon_init(const char *path, char *args, pthread_t *tid)
 {
     char daemon_path[MAX_PATH];
 
@@ -19,7 +19,41 @@ DOCKERCLI_CODE init_daemon(const char *path, char *args, pthread_t *tid)
 #ifdef __DEBUG
     printf("%s\n", daemon_path);
 #endif
-    int status = pthread_create(tid, NULL, start_daemon, daemon_path);
+    int status = pthread_create(tid, NULL, daemon_start, daemon_path);
+
+    if (status > 0)
+        return DOCKERCLIE_CANNOTINITDAE;
+
+    return DOCKERCLIE_OK;
+}
+
+void *start_daemon_healthcheck(void *args)
+{
+    daemon_res_t dres;
+    while (1) {
+        check_daemon_status();
+        parse_daemon_response(&dres, DAEMON_RES_STATUS);
+#ifdef __DEBUG
+        printf("gui: %s %s %s\n", dres.proto, dres.code, dres.code_text);
+        printf("res: %d\n", atoi(dres.code));
+#endif
+        if (atoi(dres.status->t_code) != 200)
+        /* sleep 30 sec */
+        Sleep(30000);
+    }
+
+    return NULL;
+}
+
+DOCKERCLI_CODE daemon_healthcheck(const char *path, char *args, pthread_t *tid)
+{
+    char daemon_path[MAX_PATH];
+
+    sprintf(daemon_path, "%s%s %s", path, "\\dockerd.exe", args ? args : "");
+#ifdef __DEBUG
+    printf("%s\n", daemon_path);
+#endif
+    int status = pthread_create(tid, NULL, start_daemon_healthcheck, daemon_path);
 
     if (status > 0)
         return DOCKERCLIE_CANNOTINITDAE;
